fb0723702c690fb8ae8595f238d7e497f7ca5926
Kaspar Schleiser <kaspar@schleiser.de>
2023-03-28 10:59:21 +0200

    move setup_stack to arch/

--- src/arch/cortex_m.rs
+++ src/arch/cortex_m.rs
@@ -1,8 +1,32 @@
 use core::arch::asm;
+use core::ptr::write_volatile;
 pub use cortex_m::interrupt::{self, CriticalSection, Mutex};
 use cortex_m::peripheral::SCB;
 
-use crate::sched;
+use crate::{cleanup, sched};
+
+/// Sets up stack for newly created threads.
+///
+/// After running this, the stack should look as if the thread was
+/// interrupted by an ISR. On the next return, it starts executing
+/// `func`.
+pub(crate) fn setup_stack(stack: &mut [u8], func: usize, arg: usize) -> usize {
+    let stack_start = stack.as_ptr() as usize;
+    let stack_pos = (stack_start + stack.len() - 36) as *mut usize;
+
+    unsafe {
+        write_volatile(stack_pos.offset(0), arg); // -> R0
+        write_volatile(stack_pos.offset(1), 1); // -> R1
+        write_volatile(stack_pos.offset(2), 2); // -> R2
+        write_volatile(stack_pos.offset(3), 3); // -> R3
+        write_volatile(stack_pos.offset(4), 12); // -> R12
+        write_volatile(stack_pos.offset(5), cleanup as usize); // -> LR
+        write_volatile(stack_pos.offset(6), func); // -> PC
+        write_volatile(stack_pos.offset(7), 0x01000000); // -> APSR
+    }
+
+    stack_pos as usize
+}
 
 pub fn schedule() {
     SCB::set_pendsv();
